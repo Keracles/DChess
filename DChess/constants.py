1ce778087eb5877654110662c6157d4848bbc596
Para_Noah <nomi8@orange.fr>
2022-10-11 21:33:49 +0200

    update

--- DChess/constants.py
+++ DChess/constants.py
@@ -1,29 +1,40 @@
 import pygame
 import os
 
+import pygame
+import os
+
+#Size
 Width, Height = 760,760
 Rows, Cols = 8,8
 Square = Width//Rows
 
-Brown = (87,16,16)
+#Colors
+Bg = (47,79,79)
+Black = (0,0,0)
 White = (255,255,255)
+beige = (245,245,220)
+brown_chocolate = (210,105,30)
+brown = (55,55,55)
+cornsilk = (255,248,220)
+Green = (0,255,0)
 
 path_image = "DChess\images"
 
 #Black pieces
 
-black_knight = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bkn.png")), (Square, Square))
-black_bishop = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bb.png")), (Square, Square))
-black_tower = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"br.png")), (Square, Square))
-black_king = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bk.png")), (Square, Square))
-black_queen = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bq.png")), (Square, Square))
-black_pawn = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bp.png")), (Square, Square))
+Black_Knight = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bkn.png")), (Square, Square))
+Black_Bishop = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bb.png")), (Square, Square))
+Black_Rook = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"br.png")), (Square, Square))
+Black_King = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bk.png")), (Square, Square))
+Black_Queen = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bq.png")), (Square, Square))
+Black_Pawn = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"bp.png")), (Square, Square))
 
 #White pieces
 
-white_knight = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wkn.png")), (Square, Square))
-white_bishop = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wb.png")), (Square, Square))
-white_tower = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wr.png")), (Square, Square))
-white_king = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wk.png")), (Square, Square))
-white_queen = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wq.png")), (Square, Square))
-white_pawn = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wp.png")), (Square, Square))
+White_Knight = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wkn.png")), (Square, Square))
+White_Bishop = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wb.png")), (Square, Square))
+White_Rook = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wr.png")), (Square, Square))
+White_King = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wk.png")), (Square, Square))
+White_Queen = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wq.png")), (Square, Square))
+White_Pawn = pygame.transform.scale(pygame.image.load(os.path.join(path_image,"wp.png")), (Square, Square))
--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-from operator import truediv
 import pygame
 
 from DChess.constants import *
+from DChess.game import Game
+
 
 pygame.init()
-
 clock = pygame.time.Clock()
 
 Win = pygame.display.set_mode((Width, Height))
 
-def get_position(x,y):
+def get_positions(x,y):
     row = y//Square
     col = x//Square
+
     return row,col
 
 
 def main():
     run = True
-    FPS = 60
     game_over = False
+    turn = White
+    FPS = 60
+    game = Game(Width,Height,Rows,Cols,Square,Win)
+
+
     while run:
         clock.tick(FPS)
-        Win.blit(white_bishop, (50,50))
-        pygame.display.update()
 
+        game.update_window()
+        if game.check_game():
+            game_over = True
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_SPACE and game_over:
-                    pass
+
+            if event.type == pygame.KEYDOWN and game_over:
+                if event.key == pygame.K_SPACE and game_over:
+                    game.reset()
+
+
             if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+
                 if pygame.mouse.get_pressed()[0]:
                     location = pygame.mouse.get_pos()
-                    row,col = get_position(location[0], location[1])
+                    row,col = get_positions(location[0],location[1])
+                    #print(row,col)
+                    game.select(row,col)
+
+
+
 
 main()
